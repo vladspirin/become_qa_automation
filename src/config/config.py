4129d72f2cc19607e449c50b91b848e4d27d2ea3
Vladyslav Spirin <vladspirin@hotmail.com>
2023-11-05 20:57:50 +0200

    restored the endpoint

--- src/config/config.py
+++ src/config/config.py
@@ -3,3 +3,22 @@ import os
 from typing import Any
 
 
+class OSConfigProvider():
+    @staticmethod
+    def get(item_name: str) -> Any:
+        value = os.getenv(item_name)  # get value from system envs
+        return value # return the value
+
+
+class JSONConfigProvider():
+    @staticmethod
+    def _read_config(config_path): # parse json file
+        with open(config_path) as json_file: # open json file
+            return json.load(json_file) # convert to dict/treeMap
+
+    @staticmethod
+    def get(item_name: str) -> Any:
+        value = JSONConfigProvider._read_config(
+            ".\\config\\envs\\dev.json"
+        )  # Read the file
+        return value.get(item_name) # get the value from the file by parameter name
--- tests/github/api/test_api.py
+++ tests/github/api/test_api.py
@@ -1,5 +1,25 @@
-import src.config.config
+def test_search_for_existing_repo(git_hub_api_app):
+    existing_repo_name = 'sergii'
+    repos = git_hub_api_app.search_repos(existing_repo_name)  # list/array returned
+
+    print("Checking total count is not 0")
+
+    assert repos['total_count'] != 0  # some elements exists in array/list
 
 
-def test_api():
-    requests.get(url, timeout=request_timeout)
+def test_search_for_nonexisting_repo(git_hub_api_app):
+    nonexisting_repo_name = 'lnasdklfnkjanskdjfnkjabsdjhfuyagsuygfuyagsduyfgauysdf'
+    repos = git_hub_api_app.search_repos(nonexisting_repo_name)  # list/array returned
+
+    print("Checking total count is 0")
+
+    assert repos['total_count'] == 0  # some elements exists in array/list
+
+
+def test_search_for_nonexisting_repo(git_hub_api_app):
+    nonexisting_commit_hash = 'lnasdklfnkjanskdjfnkjabsdjhfuyagsuygfuyagsduyfgauysdf'
+    repos = git_hub_api_app.search_commits(nonexisting_commit_hash)  # list/array returned
+
+    print("Checking total count is 0")
+
+    assert 1 == 0  # some elements exists in array/list
